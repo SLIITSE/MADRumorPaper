755bed4825d6d91cb341cdd80a6b2d9b8b438615
Shehanka <hcsperera@outlook.com>
2019-10-15 03:30:01 +0530

    View Emp

--- app/src/main/java/com/chamodshehanka/madrumorpaper/database/DBHandler.java
+++ app/src/main/java/com/chamodshehanka/madrumorpaper/database/DBHandler.java
@@ -7,17 +7,31 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DBHandler extends SQLiteOpenHelper {
-    public DBHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    private static final String DB_NAME = "MAD.db";
+    private static final int DB_VERSION = 2;
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + EmployeeInfo.TABLE_NAME + " (" +
+                    EmployeeInfo._ID + " PRIMARY KEY AUTOINCREMENT," +
+                    EmployeeInfo.COLUMN_1 + " TEXT," +
+                    EmployeeInfo.COLUMN_2 + " TEXT," +
+                    EmployeeInfo.COLUMN_3 + " TEXT," +
+                    EmployeeInfo.COLUMN_4 + " TEXT)";
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + EmployeeInfo.TABLE_NAME;
+
+    public DBHandler(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
     }
 }
--- app/src/main/java/com/chamodshehanka/madrumorpaper/database/EmployeeInfo.java
+++ app/src/main/java/com/chamodshehanka/madrumorpaper/database/EmployeeInfo.java
@@ -1,5 +1,14 @@
 package com.chamodshehanka.madrumorpaper.database;
 
-public class EmployeeInfo {
+import android.provider.BaseColumns;
 
+public class EmployeeInfo implements BaseColumns {
+
+    private EmployeeInfo(){}
+
+    public static final String TABLE_NAME = "EmployeeInfo";
+    public static final String COLUMN_1 = "empName";
+    public static final String COLUMN_2 = "empTelephone";
+    public static final String COLUMN_3 = "empGender";
+    public static final String COLUMN_4 = "empType";
 }
