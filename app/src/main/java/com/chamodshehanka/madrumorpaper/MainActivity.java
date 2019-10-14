9302cdbb6d09b5c3eb3f1494ac2ecece63c1338c
Shehanka <hcsperera@outlook.com>
2019-10-15 00:10:51 +0530

    Add Employee interface

--- app/src/main/java/com/chamodshehanka/madrumorpaper/MainActivity.java
+++ app/src/main/java/com/chamodshehanka/madrumorpaper/MainActivity.java
@@ -3,12 +3,41 @@ package com.chamodshehanka.madrumorpaper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAddEmployee, btnEditEmployee, btnSeachEmployee;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddEmployee = findViewById(R.id.btnAddEmployee);
+        btnEditEmployee = findViewById(R.id.btnEditEmployee);
+        btnSeachEmployee = findViewById(R.id.btnSearchEmployee);
+
+        btnAddEmployee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnEditEmployee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnSeachEmployee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
